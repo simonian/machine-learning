1deab3a76f71a183fe408343283742ff296350e5
Sergei Simonian <sersimonyan@gmail.com>
2020-01-30 19:27:02 +0300

    Complete assignment 3

--- assignment-3/ex3/countValues.m
+++ assignment-3/ex3/countValues.m
@@ -1,8 +1,15 @@
 function y = countValues(x)
+%COUNTVALUES Count number of appearance for each value from the list/array
+%   [y] = COUNTVALUES(x) 
+%   This is a helper function created by Sergei Simonian
 
 y = [];
 
 values = unique(x);
+
+% This transformation makes sure we always get [nx1] array instead of list
+values = values(:);
+
 for value = values'
     count = sum(x(:) == value);
     y = [y; value, count];
--- assignment-3/ex3/predict.m
+++ assignment-3/ex3/predict.m
@@ -20,14 +20,21 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+X1 = X;
 
+% Add ones to the X1 data matrix
+X1 = [ones(size(X1, 1), 1) X1];
 
+X2 = sigmoid(Theta1 * X1');
+X2 = X2';
 
+% Add ones to the X2 data matrix
+X2 = [ones(size(X2, 1), 1) X2];
 
+X3 = sigmoid(Theta2 * X2');
+X3 = X3';
 
-
-
-
+[v, p] = max(X3, [], 2);
 
 % =========================================================================
 
